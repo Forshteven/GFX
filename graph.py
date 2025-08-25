646973c9414ed211f2fdb69e569be9aeb80a8060
Kosikhin Nikolay <geocinds@mail.ru>
2025-08-25 18:21:18 +0900

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+
+from main import names, start_dates, finish_dates
+
+fig, ax = plt.subplots(dpi=20)
+
+width = 0.5
+color = "tab:blue"
+
+for i, event in enumerate(names):
+    ax.broken_barh([(start_dates[i], finish_dates[i] - start_dates[i])],
+                   (i - width / 2, width),
+                   facecolors=color)
+    days_word = 'день' if int((finish_dates[i] - start_dates[i]).days) % 10 == 1 and int((finish_dates[i] - start_dates[i]).days) != 11 \
+        else ('дня' if int((finish_dates[i] - start_dates[i]).days) % 10 in range(2, 5) and int((finish_dates[i] - start_dates[i]).days)
+        not in range(11, 16) else 'дней')
+    x_text = start_dates[i] + (finish_dates[i] - start_dates[i]) / 2
+    y_text = i + 0.5
+    ax.text(x_text, y_text, f'{start_dates[i]} - {finish_dates[i]}, '
+                            f'{finish_dates[i]} - {start_dates[i]} {days_word}', ha='center', va='center', fontsize=1)
+
+ax.set_yticks(range(len(names)))
+ax.set_yticklabels(names)
+start_date = min(start_dates)
+ax.set_xlim(left=start_date, right=max(finish_dates))
+ax.set_title('График')
+ax.xaxis.set_major_locator(mdates.MonthLocator())
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
+# Показать график
+plt.xticks(rotation=90, fontsize=8)
+plt.yticks(fontsize=3)
+plt.grid(True, which='both', color='black', linewidth=1)
+plt.grid(True, which='minor', linestyle=':', color='grey', linewidth=0.5)
+plt.plot()
+plt.show()
---
+++ main.py
@@ -0,0 +1,36 @@
+from openpyxl.reader.excel import load_workbook
+from datetime import datetime
+
+wb = load_workbook("График производства работ.xlsx", data_only=True)
+sheet = wb.active
+print(f"Активный лист: {sheet.title}")
+
+names = []
+start_dates = []
+finish_dates = []
+
+def pars_date(date_str):
+    return datetime.strptime(date_str, '%d-%m-%Y').date()
+
+for i in range(25, 426):
+    name = sheet["B" + str(i)].value
+    start_date = sheet[f'R{i}'].value
+    finish_date = sheet[f'S{i}'].value
+    if start_date is not None and finish_date is not None:
+        start_date = datetime.strftime(start_date, '%d-%m-%Y')
+        finish_date = datetime.strftime(finish_date, '%d-%m-%Y')
+        start_dates_parsed = pars_date(start_date)
+        finish_dates_parsed = pars_date(finish_date)
+        names.append(name)
+        start_dates.append(start_dates_parsed)
+        finish_dates.append(finish_dates_parsed)
+
+print(names)
+print(start_dates)
+print(finish_dates)
+
+
+
+
+
+
