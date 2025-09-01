7c11c74b38bcb5e88629b66b715b452ad6c2be3b
KNV <geocinds@mail.ru>
2025-09-01 15:51:21 +0900

    01.09.2025

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import re
 def pars_date(date_str):
     return datetime.strptime(date_str, '%d-%m-%Y').date()
 
-wb = load_workbook("Сводная ведомость по оборудовнаию ОМСК.xlsx", data_only=True)
+wb = load_workbook("График строительства гидроузла на 2025-2026.xlsx", data_only=True)
 sheet = wb.active
 
 names = []
@@ -14,31 +14,34 @@ start_dates = []
 finish_dates = []
 time_deltas = []
 
-for i in range(1, 40):
-    name = sheet["C" + str(i)].value
+for i in range(24, 150):
+    name = sheet["B" + str(i)].value
+    start_date = sheet[f'H{i}'].value
+    finish_date = sheet[f'I{i}'].value
+
 # Часть кода для случая, когда указаны даты начала работ и их продолжительности, но не указаны даты окончания работ
-    start_date = sheet[f'E{i}'].value
-    duration_str = sheet[f'S{i}'].value
-    if start_date is not None and duration_str is not None:
-        time_delta = re.search(r'\d{2}', duration_str)
-        days = int(time_delta.group(0))
-        start_date = datetime.strftime(start_date, '%d-%m-%Y')
-        start_dates_parsed = pars_date(start_date)
-        finish_dates_parsed = pars_date(start_date) + timedelta(days=days)
-    else:
-        continue
-# Часть кода для случая, когда указаны даты начала и окончания работ
-    # if start_date is not None and finish_date is not None:
+    # duration_str = sheet[f'I{i}'].value
+    # if start_date is not None and duration_str is not None:
+    #     time_delta = re.search(r'\d{2}', duration_str)
+    #     days = int(time_delta.group(0))
     #     start_date = datetime.strftime(start_date, '%d-%m-%Y')
-    #     finish_date = datetime.strftime(finish_date, '%d-%m-%Y')
     #     start_dates_parsed = pars_date(start_date)
-    #     finish_dates_parsed = pars_date(finish_date)
+    #     finish_dates_parsed = pars_date(start_date) + timedelta(days=days)
     # else:
     #     continue
-    #
+# Часть кода для случая, когда указаны даты начала и окончания работ
+    if  isinstance(start_date, datetime) and isinstance(finish_date, datetime):
+        start_date = datetime.strftime(start_date, '%d-%m-%Y')
+        finish_date = datetime.strftime(finish_date, '%d-%m-%Y')
+        start_dates_parsed = pars_date(start_date)
+        finish_dates_parsed = pars_date(finish_date)
+    else:
+        continue
+
     names.insert(0, name)
     start_dates.insert(0, start_dates_parsed)
     finish_dates.insert(0, finish_dates_parsed)
+
 
 
 
